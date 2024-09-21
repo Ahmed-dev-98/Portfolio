0e248f5ccacae78af4aec04ddabfc2b338fa6ce6
Ahmed Hassan <ahmed.h.mohamed98@gmail.com>
2024-09-21 19:42:55 +0300

    assets

---
+++ components/Hero.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="-top-10 left-full md:-left-32 md:-top-20 h-[80vh] w-[50vw]"
+          fill="purple"
+        />
+        <Spotlight className="top-28 left-80  h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2]  flex items-center justify-center">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+
+        <div className="flex justify-center relative my-20 z-10">
+          <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100">
+              Dynamic Web Magic with Next.js
+            </h2>
+            <TextGenerateEffect
+              words="Transforming concepts into seamless users experiences"
+              className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            />
+            <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+              Hi i&apos;m Ahmed a frontend developer from Egypt{" "}
+            </p>
+            <a href="#about">
+              <MagicButton
+                position="right"
+                icon={<FaLocationArrow />}
+                title="Show My Work"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
